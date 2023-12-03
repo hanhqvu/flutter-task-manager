f0580c40ae633a1ac244b279c3580e4c0cf81463
hanhqvu <hanhqvu@users.noreply.github.com>
2023-12-03 22:52:11 +0900

    feat: handle env var

---
+++ lib/utils/env.dart
@@ -0,0 +1,10 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(obfuscate: true, varName: 'API_URI')
+  static final String apiUri = _Env.apiUri;
+}
